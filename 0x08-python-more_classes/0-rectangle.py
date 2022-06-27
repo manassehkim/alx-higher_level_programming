a40e748401b339ad0bdad98b3ebf32acb30cbaf4
manassehkim <49023146+manassehkim@users.noreply.github.com>
2022-06-27 23:23:31 +0300

    all rectangles

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+""" A Module with an empty class """
+
+
+class Rectangle:
+    """ An empty class defining a rectangle """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+A deeper dive into classes and objects, a continuation of [0x07-python_classes]
