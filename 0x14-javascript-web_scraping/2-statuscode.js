09cde1129319ed258da355f09d2afa7d64a687ca
manassehkim <49023146+manassehkim@users.noreply.github.com>
2022-11-28 15:57:27 +0300

    statuscode

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 const request = require('request');
-const myurl = process.argv[2];
+let myurl = process.argv[2];
 
-request(myurl, function (error, response, body) {
+request(myurl, function (error, response) {
   if (error) {
     console.log(error);
   } else {
-    console.log('code: ', response.statusCode);
+    console.log('code: ' + response.statusCode);
   }
 });
